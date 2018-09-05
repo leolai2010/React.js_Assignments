4540755d993a34e0cd1f9e305ba53cf2f4f687cf
Leo <leolai2010@gmail.com>
2018-09-05 13:41:35 -0700

    Update react project

---
+++ BookList/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [ //mock data
+        { title: 'JavaScript: The Good Parts'},
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
