b1525c92023e32021bc20ae290b15c5df4b3ef20
Leo <leolai2010@gmail.com>
2018-09-06 10:51:22 -0700

    finishing up booklist project

--- BookList/src/reducers/reducer_books.js
+++ BookList/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
     return [ //mock data
-        { title: 'JavaScript: The Good Parts'},
-        { title: 'Harry Potter' },
-        { title: 'The Dark Tower' },
-        { title: 'Eloquent Ruby' }
+        { title: 'JavaScript: The Good Parts', pages: '101'},
+        { title: 'Harry Potter', pages: '258' },
+        { title: 'The Dark Tower', pages: '456' },
+        { title: 'Eloquent Ruby', pages: '357' }
     ]
 }
