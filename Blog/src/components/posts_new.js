9cf8953233ad897961945df2e3148e96f526156f
Leo <leolai2010@gmail.com>
2018-09-08 18:19:16 -0700

    Fully Functional CRUD FrontEnd React.js Application (Blog)

--- Blog/src/components/posts_new.js
+++ Blog/src/components/posts_new.js
@@ -1,32 +1,83 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+ 
 class PostsNew extends Component{
     //adding the field variable allows for tracking of changes
-    renderTitleField(field) {
+    renderField(field) {
+        const { meta: {touched, error} } = field;
+        const className = `form-group ${touched && error ? 'has-danger':''}`;
         return(
-            <div>
-                <input
+            <div className={className}>
+                <label>{field.label}</label>
+                <input className="form-control"
                     // onChange={field.input.onChange}
                     type="text"
                     {...field.input} // contains event handler and props
                 />
+                <div className="text-help">
+                    {touched ? error: ''}
+                </div>
             </div>
         );
     }
+    onSubmit(values){
+        console.log(values);
+        this.props.createPost(values, ()=>{
+            this.props.history.push('/');
+        });
+    }
     render() {
+        const{ handleSubmit } = this.props;
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field 
-                    name="title"
-                    component={this.renderTitleField} // field only know how to interact with Redux BUT not JSX
+                    label="Title"
+                    name="title" //this name has to match validation function name for it to be connected!
+                    component={this.renderField} // field only know how to interact with Redux BUT not JSX
                     // adding a component to show JSX blob
                 />
+                <Field
+                    label="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
 }
 
+function validate(values){
+    //console.log(values) -> {title:'xxx', categories:'xxx', content:'xxx'}
+    const errors = {};
+    //Validate the inputs from 'values'
+    if(!values.title || values.title.length < 3) {
+        errors.title = "Enter a title that is at least 3 characters!";
+    }
+    if(!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+    if(!values.content) {
+        errors.content = "Pleaes enter some content";
+    }
+    //If errors is empty, the form is fine to submit
+    //If errors has any properties, redux form assumes form is invalid
+    return errors;
+}
+
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm' //unique string tied to this form
-})(PostsNew);
+})(
+    connect(null,{ createPost })(PostsNew)
+);
